28c6ff2b05d919c38c1f49ffa8dad9dd2986c610
Jeffrey Brice <jeffbricemusic@gmail.com>
2013-07-15 16:48:31 -0700

    Fixed HTML forms. Added 'return false' to onsubmit (prevents page reload) Added document.forms[i].elements[i].value to push data to form Added 'Clear Form' function

--- d20Dice.js
+++ d20Dice.js
@@ -1,12 +1,12 @@
 /*jslint browser:true */
 function diceValue(min, max) {
-    "use strict";
+//    "use strict";
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var output = "You rolled dice!";
 var i = 0;
 var p = 0;
+var formHasData = false;
 var dice = Object.create(null);
 dice.sides = [0, 4, 6, 8, 10, 12, 20, 100];
 dice.num = [1, 1, 1, 1, 1, 1, 1, 1];
@@ -18,33 +18,34 @@ dice.multi = 1;
 dice.average = 0;
 
 //Grabs values from HTML form
-dice.num[0] = document.diceForm.numCust.value;
-dice.num[1] = document.diceForm.d4.value;
-dice.num[2] = document.diceForm.d6.value;
-dice.num[3] = document.diceForm.d8.value;
-dice.num[4] = document.diceForm.d10.value;
-dice.num[5] = document.diceForm.d12.value;
-dice.num[6] = document.diceForm.d20.value;
-dice.num[7] = document.diceForm.d100.value;
-dice.sides[1] = document.diceForm.sidesCust.value;
+dice.num[7] = document.forms[0].elements[0].value;
+dice.num[6] = document.forms[0].elements[1].value;
+dice.num[5] = document.forms[0].elements[2].value;
+dice.num[4] = document.forms[0].elements[3].value;
+dice.num[3] = document.forms[0].elements[4].value;
+dice.num[2] = document.forms[0].elements[5].value;
+dice.num[1] = document.forms[0].elements[6].value;
+dice.num[0] = document.forms[0].elements[7].value;
+dice.sides[0] = document.forms[0].elements[8].value;
 
 function rollDice() {
-    "use strict";
+//    "use strict";
     for (i = 0; i < dice.num.length; i++) {
         dice.sum = 0;
-        dice.each = [];
+        dice.each.length = 0;
         dice.high = 0;
         dice.low = (dice.sides[i] + 1);
-        dice.multi = 1;
-        output = output + ("<p>You rolled " + dice.num[i] + " D" + dice.sides[i] + "'s</p>");   
+        dice.multi = 1;  
+        dice.average = 0;
         
         while (dice.num[i] > 0) { 
-            dice.sum = dice.sum + (diceValue(1, (dice.sides[i] + 1)));
+            dice.sum = dice.sum + (diceValue(1, (dice.sides[i])));
             dice.each.push(diceValue(1, (dice.sides[i] + 1)));
-            dice.multi = dice.multi * (diceValue(1, (dice.sides[i] + 1)));
+            dice.multi = dice.multi * (diceValue(1, (dice.sides[i])));
             dice.num[i]--;
-            dice.average = (dice.sum/dice.num.length);
             }
+      
+        dice.average = (dice.sum/dice.each.length);
 
         for (p = 0; p < dice.each.length; p++) {
             if (dice.each[p] > dice.high) {
@@ -54,14 +55,28 @@ function rollDice() {
                 dice.low = dice.each[p];
             }
         }
-/* Places output in proper output areas. Need to figure out the proper way to change the name to
-the proper form sections.
-	document.output.d(dice.sides[i])Sum.value = dice.sum;
-	document.output.d(dice.sides[i])Array.value = dice.each;
-	document.output.d(dice.sides[i])High.value = dice.high;
-	document.output.d(dice.sides[i])Low.value = dice.low;
-	document.output.d(dice.sides[i])Multi.value = dice.multi;
-	document.output.d(dice.sides[i])Average.value = dice.average; */
+// Places output in proper output areas.
+      document.forms[2].elements[i].value = dice.sum;
+      document.forms[3].elements[i].value = dice.each;
+      document.forms[4].elements[i].value = dice.high;
+      document.forms[5].elements[i].value = dice.low;
+      document.forms[6].elements[i].value = dice.multi;
+      document.forms[7].elements[i].value = dice.average;
+    }
+  formHasData = true;
+  console.log(dice.num);
+}
+
+function clearForm() {
+    if (formHasData) {
+        for (i = 0; i < 7; i++) {
+            document.forms[2].elements[i].value = "";
+            document.forms[3].elements[i].value = "";
+            document.forms[4].elements[i].value = "";
+            document.forms[5].elements[i].value = "";
+            document.forms[6].elements[i].value = "";
+            document.forms[7].elements[i].value = "";
+        }
     }
 }
 
