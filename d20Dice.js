2d704aa5d8d24fd6a552df7e380c80eda640b266
Jeffrey Brice <jeffbricemusic@gmail.com>
2013-07-16 12:38:39 -0700

    Rearranged html forms so dice data populates the proper boxes Fixed rollDice function so that all calculations work properly

--- d20Dice.js
+++ d20Dice.js
@@ -1,57 +1,51 @@
 /*jslint browser:true */
+// Determines value of dice as they are rolled
 function diceValue(min, max) {
-//    "use strict";
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    "use strict";
+    return Math.floor((Math.random() * max) + min);
 }
 
+// Variables
 var i = 0;
-var p = 0;
 var formHasData = false;
 var dice = Object.create(null);
-dice.sides = [0, 4, 6, 8, 10, 12, 20, 100];
-dice.num = [1, 1, 1, 1, 1, 1, 1, 1];
+dice.sides = [1, 4, 6, 8, 10, 12, 20, 100];
+dice.num = [10, 4, 4, 4, 4, 4, 4, 4];
 dice.sum = 0;
-dice.each = [];
-dice.high = 0;
+dice.each = new Array();
+dice.high = null;
 dice.low = (dice.sides[i] + 1);
 dice.multi = 1;
-dice.average = 0;
-
-//Grabs values from HTML form
-dice.num[7] = document.forms[0].elements[0].value;
-dice.num[6] = document.forms[0].elements[1].value;
-dice.num[5] = document.forms[0].elements[2].value;
-dice.num[4] = document.forms[0].elements[3].value;
-dice.num[3] = document.forms[0].elements[4].value;
-dice.num[2] = document.forms[0].elements[5].value;
-dice.num[1] = document.forms[0].elements[6].value;
-dice.num[0] = document.forms[0].elements[7].value;
-dice.sides[0] = document.forms[0].elements[8].value;
+dice.average = null;
+dice.value = 0;
 
 function rollDice() {
-//    "use strict";
-    for (i = 0; i < dice.num.length; i++) {
+    "use strict";
+    for (var i = 0; i < dice.num.length; i++) {
         dice.sum = 0;
         dice.each.length = 0;
         dice.high = 0;
         dice.low = (dice.sides[i] + 1);
         dice.multi = 1;  
-        dice.average = 0;
-        
-        while (dice.num[i] > 0) { 
-            dice.sum = dice.sum + (diceValue(1, (dice.sides[i])));
-            dice.each.push(diceValue(1, (dice.sides[i] + 1)));
-            dice.multi = dice.multi * (diceValue(1, (dice.sides[i])));
+
+// Determines sum, all dice multiplied together and the value of each dice rolled        
+        while (dice.num[i] > 0) {
+            dice.value = diceValue(1, dice.sides[i]);
+            dice.each.push(dice.value);
+            dice.multi = dice.multi * dice.value;
+            dice.sum = dice.sum + dice.value;            
             dice.num[i]--;
-            }
-      
+            }    
+
+// Determines the average of all dice rolled      
         dice.average = (dice.sum/dice.each.length);
 
-        for (p = 0; p < dice.each.length; p++) {
+// Determines lowest & highest dice
+        for (var p = 0; p < dice.each.length; p++) {
             if (dice.each[p] > dice.high) {
                 dice.high = dice.each[p];
             }
-            if (dice.each[p] < dice.low) {
+            else if (dice.each[p] < dice.low) {
                 dice.low = dice.each[p];
             }
         }
@@ -62,14 +56,22 @@ function rollDice() {
       document.forms[5].elements[i].value = dice.low;
       document.forms[6].elements[i].value = dice.multi;
       document.forms[7].elements[i].value = dice.average;
+      console.log("Sum " + dice.sides[i] +":" + dice.sum);
+      console.log("Each " + dice.sides[i] +":" + dice.each);
+      console.log("Length " + dice.each.length);
+      console.log("High " + dice.sides[i] +":" + dice.high);
+      console.log("Low " + dice.sides[i] +":" + dice.low);
+      console.log("Multi " + dice.sides[i] +":" + dice.multi);
+      console.log("Average " + dice.sides[i] +":" + dice.average);       
     }
+// Tells the clearForm function that the tables have roll data in them    
   formHasData = true;
-  console.log(dice.num);
 }
 
+// Clears tables after a roll has occurred, otherwise does nothing
 function clearForm() {
     if (formHasData) {
-        for (i = 0; i < 7; i++) {
+        for (var k = 0; i < 7; i++) {
             document.forms[2].elements[i].value = "";
             document.forms[3].elements[i].value = "";
             document.forms[4].elements[i].value = "";
